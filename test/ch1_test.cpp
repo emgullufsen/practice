dec7a652f26ef0d38678191a94ae9594cb11506f
emgullufsen <ericgullufsen@gmail.com>
2022-03-03 10:38:41 -0900

    test rigged for ch1_q3 with EXPECT_STREQ

--- test/ch1_test.cpp
+++ test/ch1_test.cpp
@@ -22,6 +22,18 @@ protected:
 	void TearDown() override {}
 };
 
+struct ch1_fix2 : public ::testing::Test {
+	std::vector<std::tuple<char *, char *>> test_data;
+	char a[7] = {'E','V','A','N','N','E','\0'};
+	char a_r[7] = {'E','N','N','A','V','E','\0'}; 
+	ch1_fix2() {
+		test_data.push_back(std::tuple<char *, char *>(a, a_r));
+	}
+protected:
+	void SetUp() override {}
+	void TearDown() override {}
+};
+
 struct ch1_fix3 : public ::testing::Test {
 	std::vector<std::tuple<std::string, std::string>> test_data; 
 	ch1_fix3() {
@@ -41,6 +53,12 @@ protected:
 TEST_F(ch1_fix1, ch1_q1_unique_chars){
 	EXPECT_EQ(practice::unique_chars(std::get<0>(test_data[0])),
 		std::get<1>(test_data[0]));
+}
+
+TEST_F(ch1_fix2, ch1_q2_reverse_cstr){
+	for (auto [dat, ret] : test_data){
+		EXPECT_STREQ(practice::reverse_cstr(dat), ret);
+	}
 }
 
 TEST_F(ch1_fix3, ch1_q3_remove_dups){
