d12b67bc1be1abe400e1857d3c639530c0c2b1f8
emgullufsen <ericgullufsen@gmail.com>
2022-03-08 22:36:42 -0900

    nada just wrote out Q

--- src/ch1.cpp
+++ src/ch1.cpp
@@ -92,4 +92,12 @@ replace_spaces(std::string& inp){
 	}
 	return inp;
 }
-}
+
+/*
+* 1.6 Given an image represented by an NxN matrix, where each pixel in the image
+* is 4 bytes, write a method to rotate the image by 90 degrees. Can you do this
+* in place?
+*/
+
+
+} // namespace practice
