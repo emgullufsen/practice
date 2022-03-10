c667d33b137f8c12d4449f2531f32a8b2abcfd61
Gullufsen, Eric M (EED) <eric.gullufsen@alaska.gov>
2022-03-10 09:37:11 -0900

    sweet we run on windows now

---
+++ test/build_line_windows.ps1
@@ -0,0 +1,1 @@
+clang++ -std=c++17 -I..\include\ -I'C:\Users\emgullufsen\source\repos\googletest\googletest\include\' -L'C:\Users\emgullufsen\source\repos\googletest\build\lib\Release\' .\main_test.cpp .\ch1_test.cpp ..\src\ch1.cpp -o test_win_ex.exe -lgtest
