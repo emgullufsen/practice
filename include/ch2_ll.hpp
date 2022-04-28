a3d2d9df608f59ebf793ac5c9430938b3cf48f5d
emgullufsen <ericgullufsen@gmail.com>
2022-04-27 21:53:35 -0800

    working ll impl bare bones

--- include/ch2_ll.hpp
+++ include/ch2_ll.hpp
@@ -9,13 +9,30 @@ public:
 	LLNode(T val) : data(val), next(nullptr), prev(nullptr) {}
 	LLNode<T> *next, *prev;
 	T data;
-}
+};
 
 template<typename T>
 class LList {
 public:
 	LList() : head(nullptr) {}
 	LLNode<T> *head;
-	insert_node(LLNode<T>);
+	void insert_node(LLNode<T> *);
+};
+
+template<typename T>
+void LList<T>::insert_node(LLNode<T>* lln){
+	if (this->head == nullptr){
+		this->head = lln;
+		return;
+	}
+	
+	LLNode<T> *llc = this->head;
+	while (llc->next != nullptr){
+		llc = llc->next;
+	}
+	llc->next = lln;
+	lln->next = nullptr; // ensure for sanity
+}
+
 }
 #endif
